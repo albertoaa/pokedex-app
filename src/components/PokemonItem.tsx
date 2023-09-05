0a1981c63019e9d17271a9fb0db80460703d07a3
Alberto Aragon Alvarez <ronin8503@gmail.com>
2023-09-05 21:05:31 +0100

    pokemon cards and extra info

--- src/components/PokemonItem.tsx
+++ src/components/PokemonItem.tsx
@@ -1,36 +1,112 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Pokemon } from '../types';
 
 interface PokemonItemProps {
   pokemon: Pokemon;
-  key: number;
 }
 
-export const PokemonItem: React.FC<PokemonItemProps> = ({ key, pokemon }) => {
+export const PokemonItem: React.FC<PokemonItemProps> = ({ pokemon }) => {
   const [pokemonPicture, setPokemonPicture] = useState<string>('');
+  const [pokemonAbilities, setPokemonAbilities] = useState<string[]>([]);
+  const [pokemonTypes, setPokemonTypes] = useState<string[]>([]);
+  const [pokemonMoves, setPokemonMoves] = useState<string[]>([]);
+  const [isFlipped, setIsFlipped] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchPokemonPicture = async () => {
       const response = await fetch(pokemon.url);
       const data = await response.json();
+      // console.log(data);
       setPokemonPicture(data.sprites.front_default);
+      setPokemonAbilities(data.abilities);
+      setPokemonTypes(data.types);
+      setPokemonMoves(data.moves);
     };
 
     fetchPokemonPicture();
-  }, []);
+  }, [pokemon.url]);
+
+  const handleFlip = () => {
+    setIsFlipped(!isFlipped);
+  };
+
+  const getRandomMoves = (moves: string[], qty: number): string[] => {
+    const randomMoves = [];
+    for (let i = 0; i < qty; i++) {
+      const randomIndex = Math.floor(Math.random() * moves.length);
+      randomMoves.push(moves[randomIndex]);
+    }
+    return randomMoves;
+  };
 
   return (
-    <div
-      key={key}
-      className="m-4 h-60 w-40 rounded-md border-2 border-black bg-slate-300"
-    >
+    <div className="w-70 m-4 h-80 rounded-md border-2 border-black bg-slate-300">
       <div className="info h-3/4 flex-col items-center justify-center justify-self-center">
-        <img src={pokemonPicture} alt={pokemon.name} className="m-auto w-4/5" />
-        <p className="text-center">{pokemon.name}</p>
+        {!isFlipped ? (
+          <div className="card-face">
+            <img
+              src={pokemonPicture}
+              alt={pokemon.name}
+              className="m-auto w-4/5"
+            />
+            <p className="text-center">{pokemon.name}</p>
+          </div>
+        ) : (
+          <div className="card-face back-face">
+            <h3 className="text-center text-xl font-bold">{pokemon.name}</h3>
+            <div className="ml-2 w-4/5">
+              <h4 className="text-md text-start font-semibold underline">
+                Abilities
+              </h4>
+              <p className="ml-2 list-none">
+                {pokemonAbilities.map((ability, index) => (
+                  <span key={index} className="pr-2">
+                    {ability.ability.name}
+                  </span>
+                ))}
+              </p>
+            </div>
+            <div className="ml-2 w-4/5">
+              <h4 className="text-md text-start font-semibold underline">
+                Types
+              </h4>
+              <p className="ml-2 list-none">
+                {pokemonTypes.map((type, index) => (
+                  <span className="pr-2" key={index}>
+                    {type.type.name}
+                  </span>
+                ))}
+              </p>
+            </div>
+            <div className="ml-2 w-4/5">
+              <h4 className="text-md text-start font-semibold underline">
+                Moves
+              </h4>
+              <p className="ml-2 list-none">
+                {getRandomMoves(pokemonMoves, 5).map((move, index) => (
+                  <span className="pr-2" key={index}>
+                    {move.move.name}
+                  </span>
+                ))}
+              </p>
+            </div>
+          </div>
+        )}
       </div>
-      <div className="flex h-1/4 items-center justify-evenly">
-        <button className="bg-blue-300 px-4 py-2 text-gray-800">View</button>
-        <button className="bg-sky-800 px-4 py-2 text-white">Catch</button>
+      <div
+        className={`flex h-1/4 items-center justify-evenly ${
+          isFlipped ? 'flipped' : ''
+        }`}
+      >
+        <button
+          className="mx-1 w-16 bg-blue-300 px-2 py-2 text-gray-800"
+          onClick={handleFlip}
+        >
+          {isFlipped ? 'Back' : 'Info'}
+        </button>
+        <button className="mx-1 w-16 bg-sky-800 px-2 py-2 text-white">
+          Catch
+        </button>
       </div>
     </div>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ export const Home = () => {
   const [allPokemons, setAllPokemons] = useState<types.Pokemon[]>([]);
 
   const fetchAllPokemons = async () => {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=50');
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=18');
     const data = await response.json();
     setAllPokemons(data.results);
   };
@@ -21,6 +21,16 @@ export const Home = () => {
       <header className="bg-blue-300 p-4">
         <h1 className="text-3xl text-white">Pokedex APP</h1>
       </header>
+      <section className="search-bar flex w-2/5 p-4">
+        <input
+          type="text"
+          id="first_name"
+          className="block w-full rounded-lg border border-gray-300 bg-blue-200 p-2.5 text-sm text-black placeholder-gray-700 focus:border-blue-500 focus:ring-blue-500"
+          placeholder="Search your favorite Pokemon by Name"
+          required
+        />
+        <button className="mx-4 bg-sky-800 px-4 py-2 text-white">Search</button>
+      </section>
       <section className="grid grid-cols-6 items-center justify-center gap-2">
         {allPokemons.map((pokemon, index) => (
           <PokemonItem key={index} pokemon={pokemon} />
