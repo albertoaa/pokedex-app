c10823ae06b6339bf2c146392c43f27375063efa
Alberto Aragon Alvarez <ronin8503@gmail.com>
2023-09-05 16:43:33 +0100

    adding actions buttons

--- src/components/PokemonItem.tsx
+++ src/components/PokemonItem.tsx
@@ -22,10 +22,16 @@ export const PokemonItem: React.FC<PokemonItemProps> = ({ key, pokemon }) => {
   return (
     <div
       key={key}
-      className="m-4 h-40 w-40 flex-col items-center justify-center justify-self-center rounded-md border-2 border-black bg-slate-300"
+      className="m-4 h-60 w-40 rounded-md border-2 border-black bg-slate-300"
     >
-      <img src={pokemonPicture} alt={pokemon.name} className="m-auto w-4/5" />
-      <p className="text-center">{pokemon.name}</p>
+      <div className="info h-3/4 flex-col items-center justify-center justify-self-center">
+        <img src={pokemonPicture} alt={pokemon.name} className="m-auto w-4/5" />
+        <p className="text-center">{pokemon.name}</p>
+      </div>
+      <div className="flex h-1/4 items-center justify-evenly">
+        <button className="bg-blue-300 px-4 py-2 text-gray-800">View</button>
+        <button className="bg-sky-800 px-4 py-2 text-white">Catch</button>
+      </div>
     </div>
   );
 };
